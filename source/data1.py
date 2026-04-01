8ad17a6a19635be6145d4da3f6b58a0f33aae46a
yoda1236 <tymon.rosinski16@gmail.com>
2026-04-01 13:02:57 +0200

    Dodałem funkcję która pozwala użytkownikom wybierać z jakich plików chcą korzystać, również możliwość wybierania dowolnej liczby plików z których się korzysta

--- source/data1.py
+++ source/data1.py
@@ -27,33 +27,43 @@ higgs = pd.read_csv("D:\Praktyki_zawodowe\Htt_plotter/data/Higgs.csv")
 # print(sprawdzanie("METx", "trueMETx"))
 
 class Plotter:
-    def __init__(self, xlim_contrl = None, xlim_resolution = None, bins = None, alpha = None):
-        if xlim_contrl is None:
-            self.xlim_ctrl = int(input("Set xlim for control: "))
-        else:
-            self.xlim_ctrl = xlim_contrl
-        
-        if xlim_resolution is None:
-            self.xlim_resol = int(input("Set xlim for resolution: "))
-        else:
-            self.xlim_resol = xlim_resolution
-
+    def __init__(self, xlim_contrl=None, xlim_resolution=None, bins=None, alpha=None):
+        self.xlim_ctrl = xlim_contrl or int(input("Set xlim for control: "))
+        self.xlim_resol = xlim_resolution or int(input("Set xlim for resolution: "))
         self.bins = bins
         self.alpha = alpha
         self.recon = []
         self.contr = []
         self.base_path = Path(__file__).resolve().parent.parent
+        self.all_data = []
 
-    def load_data(self, relative_path, secnd_pth):
-        full_path = self.base_path / relative_path
+    def load_data(self, *relative_paths):
+        self.all_paths = []
+        self.all_data = []
 
-        if full_path.exists():
-            self.data = pd.read_csv(full_path)
+        if len(relative_paths) == 0:
+            relative_paths_list = []
+            while True:
+                new = input("Add new path (end to finish): ")
+                if new.lower() == "end":
+                    break
+                relative_paths_list.append("data\\" +new)
+            relative_paths = relative_paths_list
 
-        second_path = self.base_path / secnd_pth
+        for rel_path in relative_paths:
+            full_path = self.base_path / rel_path
+            self.all_paths.append(rel_path)
 
-        if second_path.exists():
-            self.data2 = pd.read_csv(second_path)
+            if full_path.exists():
+                try:
+                    df = pd.read_csv(full_path)
+                    self.all_data.append({"name": full_path.name, "data": df})
+                    print(f"Loaded: {full_path.name}")
+                except Exception as e:
+                    print(f"Error while loading {full_path.name}: {e}")
+            else:
+                print(f"File {full_path} does not exist")
+            
 
 
 
@@ -74,136 +84,147 @@ class Plotter:
         else:
             self.batch_size = batch_size
 
-        datasets = []
-        if hasattr(self, 'data'):
-            datasets.append((self.data, self.contr_name, self.recon_name))
-        if hasattr(self, 'data2'):
-            datasets.append((self.data2, self.contr_name, self.recon_name))
-
         self.batch_dfs = []
 
-        for data, contr_name, recon_name in datasets:
-            contr = data[contr_name]
-            recon = data[recon_name]
+        for item in self.all_data:
+            df = item["data"]
 
-            resolution = (recon - contr) / contr
-            resolution = resolution.replace([np.inf, -np.inf], np.nan).dropna()
+            if self.contr_name in df.columns and self.recon_name in df.columns:
+                contr = df[self.contr_name]
+                recon = df[self.recon_name]
 
-            batches = [
-                resolution.iloc[i:i+self.batch_size].reset_index(drop=True)
-                for i in range(0, len(resolution), self.batch_size)
-            ]
+                resolution = (recon - contr) / contr
+                resolution = resolution.replace([np.inf, -np.inf], np.nan).dropna()
 
-            batch_df = pd.concat(batches, axis=1)
-            self.batch_dfs.append(batch_df)
+                batches = [
+                    resolution.iloc[i:i+self.batch_size].reset_index(drop=True)
+                    for i in range(0, len(resolution), self.batch_size)
+                ]
+
+                batch_df = pd.concat(batches, axis=1)
+                self.batch_dfs.append(batch_df)
+            else:
+                print(f"Column {self.contr_name} or {self.recon_name} does not exist")
 
 
     def control_plot(self):
-        contr = self.data[self.contr_name]
-        contr2 = self.data2[self.contr_name]
-
+        if len(self.all_data) == 0:
+            print("No data in all_data")
+            return
 
         folder_name = "control_plots"
-        if not os.path.exists(folder_name):
-            os.mkdir(folder_name)
-            print(f"Folder '{folder_name}' created.")
-        else:
-            print(f"Folder '{folder_name}' already exist.")
+        os.makedirs(folder_name, exist_ok=True)
 
-        counts1, bins = np.histogram(
-            contr,
-            bins=self.bins,
-            range=(-self.xlim_ctrl, self.xlim_ctrl)
-        )
+        colors = ["blue", "lime", "red", "orange", "purple", "cyan", "magenta"]
 
-        counts2, bins = np.histogram(
-            contr2,
-            bins=self.bins,
-            range=(-self.xlim_ctrl, self.xlim_ctrl)
-        )
+        plt.figure()
+        bottom_counts = None
+        labels = []
 
-        width=np.diff(bins)
+        for i, item in enumerate(self.all_data):
+            df = item["data"]
+            file_name = item["name"]
+            if self.contr_name not in df.columns:
+                print(f"File {file_name} does not contain {self.contr_name}, next")
+                continue
 
-        plt.bar(
-            bins[:-1],
-            counts1,
-            width=width,
-            edgecolor="black",
-            align="edge",
-            color = "blue"
-        )
+            contr = df[self.contr_name]
 
-        plt.bar(
-            bins[:-1],
-            counts2,
-            width = width,
-            bottom=counts1,
-            color="lime",
-            edgecolor="black",
-            align="edge",
-            label=f"Control from Z0 - {self.recon_name}"
-        )
+            counts, bins = np.histogram(
+                contr,
+                bins=self.bins,
+                range=(-self.xlim_ctrl, self.xlim_ctrl)
+            )
 
-        file_path = os.path.join(folder_name, f"{self.contr_name}.png")
+            width = np.diff(bins)
+
+            if bottom_counts is None:
+                bottom_counts = np.zeros_like(counts)
+
+            plt.bar(
+                bins[:-1],
+                counts,
+                width=width,
+                bottom=bottom_counts,
+                color=colors[i % len(colors)],
+                edgecolor="black",
+                align="edge",
+            )
+
+            bottom_counts += counts
+            labels.append(f"Control for {file_name} - {self.recon_name}")
 
         plt.title(f"Control {self.contr_name} histogram")
         plt.xlabel(self.contr_name)
-        plt.legend([f"Control from Higgs - {self.recon_name}", f"Control from Z0 - {self.recon_name}"], loc="upper right")
+        plt.legend(labels, loc="upper right")
+
+        file_path = os.path.join(folder_name, f"{self.contr_name}.png")
         plt.savefig(file_path)
         plt.clf()
-        print(f"File saved to {self.contr_name}.png")
+        print(f"File saved to {file_path}")
 
     def resolution_plot(self):
 
+        if not hasattr(self, "batch_dfs") or len(self.batch_dfs) == 0:
+            print("No batches for histogram")
+            return
+
         folder_name = "resolution_plots"
-        if not os.path.exists(folder_name):
-            os.mkdir(folder_name)
-            print(f"Folder '{folder_name}' created.")
-        else:
-            print(f"Folder '{folder_name}' already exist.")
+        os.makedirs(folder_name, exist_ok=True)
 
-        counts, bins = np.histogram(
-            self.batch_dfs[0],
-            bins=self.bins,
-            range=(-self.xlim_resol, self.xlim_resol)
-        )
+        colors = ["blue", "lime", "red", "orange", "purple", "cyan", "magenta"]
 
-        counts2, bins = np.histogram(
-            self.batch_dfs[1],
-            bins=self.bins,
-            range=(-self.xlim_resol, self.xlim_resol)
-        )
+        plt.figure()
 
-        plt.bar(
-            bins[:-1],
-            counts,
-            width=np.diff(bins),
-            edgecolor="black",
-            align="edge"
-        )
+        bottom_counts = None
 
-        plt.bar(
-            bins[:-1],
-            counts2,
-            bottom=counts, 
-            color="lime", 
-            edgecolor="black", 
-            width=np.diff(bins),
-            align="edge"
-        )
+        for i, batch_df in enumerate(self.batch_dfs):
+            if hasattr(batch_df, "values"):
+                values = batch_df.values.flatten()
+            else:
+                values = batch_df
 
-        file_path = os.path.join(folder_name, f"Test_{self.recon_name}_from_{self.contr_name}.png")
+            counts, bins = np.histogram(
+                values,
+                bins=self.bins,
+                range=(-self.xlim_resol, self.xlim_resol)
+            )
+
+            width = np.diff(bins)
+
+            if bottom_counts is None:
+                bottom_counts = np.zeros_like(counts)
+
+
+            plt.bar(
+                bins[:-1],
+                counts,
+                width=width,
+                bottom=bottom_counts,
+                color=colors[i % len(colors)],
+                edgecolor="black",
+                align="edge",
+            )
+
+            bottom_counts += counts
 
         plt.title(f"Resolution {self.recon_name} histogram")
-        plt.legend(
-            [f"Reconstruction for Higgs - {self.recon_name}", f"Reconstruction for Z0 - {self.recon_name}"],
-            loc="upper right")
+        plt.xlabel(self.recon_name)
+        y = 0
+        labels = []
+        for y, path in enumerate(self.all_paths):
+            file_name = Path(path).name
+            labels.append(f"Reconstruction for {file_name} - {self.recon_name}")
+            y+=1
+        plt.legend(labels, loc="upper right")
+
+        file_path = os.path.join(folder_name, f"Test_{self.recon_name}_from_{self.contr_name}.png")
         plt.savefig(file_path)
         plt.clf()
-        print(f"File saved to Test_{self.recon_name}_from_{self.contr_name}.png")
+        print(f"File saved to {file_path}")
 
 object = Plotter(100, 50, 20, 1)
-print(object.load_data("data/Higgs.csv", "data/Z0.csv"))
+print(object.load_data())
 print(object.set_parameters("trueMETx", "METx"))
 print(object.batch(250))
 print(object.control_plot())
